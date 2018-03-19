12b517dadfedba987b193072ce6e1cc8fba0d431
MoatazGitHub <moataz.mohammady@gmail.com>
2018-03-19 22:37:41 +0200

    Create angularjs Post component

---
+++ public/js/component/posts/ctrl.js
@@ -0,0 +1,102 @@
+angular.module('app').component('posts', {
+    bindings: {
+        data: '='
+    },
+    templateUrl: 'js/component/posts/view.html',
+    controller: function ($http) {
+        var ctrl = this;
+        ctrl.posts = [];
+        // List posts
+        ctrl.loadPosts = function () {
+            $http.get('/post')
+                .then(function success(e) {
+                    ctrl.posts = e.data.posts;
+                    debugger
+                });
+        };
+        ctrl.loadPosts();
+
+        ctrl.errors = [];
+
+        ctrl.post = {
+            name: '',
+            description: ''
+        };
+        ctrl.initPost = function () {
+            debugger
+            ctrl.resetForm();
+            $("#add_new_post").modal('show');
+        };
+        ctrl.$onInit = function() {
+
+        };
+        // Add new Post
+        ctrl.addPost = function () {
+            $http.post('/post', {
+                title: ctrl.post.title,
+                description: ctrl.post.description
+            }).then(function success(e) {
+                ctrl.resetForm();
+                ctrl.posts.push(e.data.post);
+                $("#add_new_post").modal('hide');
+
+            }, function error(error) {
+                ctrl.recordErrors(error);
+            });
+        };
+
+        ctrl.recordErrors = function (error) {
+            debugger
+            ctrl.errors = [];
+            if (error.data.errors.title) {
+                ctrl.errors.push(error.data.errors.title[0]);
+            }
+
+            if (error.data.errors.description) {
+                ctrl.errors.push(error.data.errors.description[0]);
+            }
+        };
+
+        ctrl.resetForm = function () {
+            ctrl.post.name = '';
+            ctrl.post.description = '';
+            ctrl.errors = [];
+        };
+
+
+        // initialize update action
+        ctrl.initEdit = function (index) {
+            ctrl.errors = [];
+            ctrl.edit_post = ctrl.posts[index];
+            $("#edit_post").modal('show');
+        };
+
+        // update the given post
+        ctrl.updatePost = function () {
+            $http.patch('/post/' + ctrl.edit_post.id, {
+                title: ctrl.edit_post.title,
+                description: ctrl.edit_post.description
+            }).then(function success(e) {
+                ctrl.errors = [];
+                $("#edit_post").modal('hide');
+            }, function error(error) {
+                ctrl.recordErrors(error);
+            });
+        };
+
+
+        ctrl.deletePost = function (index) {
+
+            var conf = confirm("Do you really want to delete vm post?");
+
+            if (conf === true) {
+                $http.delete('/post/' + ctrl.posts[index].id)
+                    .then(function success(e) {
+                        ctrl.posts.splice(index, 1);
+                    });
+            }
+        };
+
+    },
+})
+
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,6 +5,7 @@
  */
 
 require('./bootstrap');
+
 //
 // window.Vue = require('vue');
 //
@@ -20,110 +20,119 @@ require('./bootstrap');
 // const app = new Vue({
 //     el: '#app'
 // });
-
 // import './bootstrap';
-import 'angular';
+// import 'angular';
 
-var app = angular.module('LaravelCRUD', []
+
+var app = angular.module('app', ['ui.router']
     , ['$httpProvider', function ($httpProvider) {
         $httpProvider.defaults.headers.post['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
     }]);
 
 
-app.controller('PostController', ['$scope', '$http', function ($scope, $http) {
-
-    $scope.posts = [];
-
-    // List posts
-    $scope.loadPosts = function () {
-        $http.get('/post')
-            .then(function success(e) {
-                $scope.posts = e.data.posts;
-                
-            });
-    };
-    $scope.loadPosts();
-
-
-    $scope.errors = [];
-
-    $scope.post = {
-        name: '',
-        description: ''
-    };
-    $scope.initPost = function () {
-        
-        $scope.resetForm();
-        $("#add_new_post").modal('show');
-    };
-
-    // Add new Post
-    $scope.addPost = function () {
-        $http.post('/post', {
-            title: $scope.post.title,
-            description: $scope.post.description
-        }).then(function success(e) {
-            $scope.resetForm();
-            $scope.posts.push(e.data.post);
-            $("#add_new_post").modal('hide');
-
-        }, function error(error) {
-            $scope.recordErrors(error);
-        });
-    };
-
-    $scope.recordErrors = function (error) {
-        debugger
-        $scope.errors = [];
-        if (error.data.errors.title) {
-            $scope.errors.push(error.data.errors.title[0]);
-        }
-
-        if (error.data.errors.description) {
-            $scope.errors.push(error.data.errors.description[0]);
-        }
-    };
-
-    $scope.resetForm = function () {
-        $scope.post.name = '';
-        $scope.post.description = '';
-        $scope.errors = [];
-    };
-
-
-
-    // initialize update action
-    $scope.initEdit = function (index) {
-        $scope.errors = [];
-        $scope.edit_post = $scope.posts[index];
-        $("#edit_post").modal('show');
-    };
-
-    // update the given post
-    $scope.updatePost = function () {
-        $http.patch('/post/' + $scope.edit_post.id, {
-            title: $scope.edit_post.title,
-            description: $scope.edit_post.description
-        }).then(function success(e) {
-            $scope.errors = [];
-            $("#edit_post").modal('hide');
-        }, function error(error) {
-            $scope.recordErrors(error);
-        });
-    };
-
-
-    $scope.deletePost = function (index) {
-
-        var conf = confirm("Do you really want to delete this post?");
-
-        if (conf === true) {
-            $http.delete('/post/' + $scope.posts[index].id)
-                .then(function success(e) {
-                    $scope.posts.splice(index, 1);
-                });
-        }
-    };
-
-
-}]);
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+    // HOME STATES AND NESTED VIEWS ========================================
+        .state('posts', {
+            url: '/posts',
+            template: '<posts></posts>'
+        })
+});
+//
+//
+// app.controller('PostController', ['$scope', '$http', function ($scope, $http) {
+//
+//     $scope.posts = [];
+//     // List posts
+//     $scope.loadPosts = function () {
+//         $http.get('/post')
+//             .then(function success(e) {
+//                 $scope.posts = e.data.posts;
+//
+//             });
+//     };
+//     $scope.loadPosts();
+//
+//
+//     $scope.errors = [];
+//
+//     $scope.post = {
+//         name: '',
+//         description: ''
+//     };
+//     $scope.initPost = function () {
+//
+//         $scope.resetForm();
+//         $("#add_new_post").modal('show');
+//     };
+//
+//     // Add new Post
+//     $scope.addPost = function () {
+//         $http.post('/post', {
+//             title: $scope.post.title,
+//             description: $scope.post.description
+//         }).then(function success(e) {
+//             $scope.resetForm();
+//             $scope.posts.push(e.data.post);
+//             $("#add_new_post").modal('hide');
+//
+//         }, function error(error) {
+//             $scope.recordErrors(error);
+//         });
+//     };
+//
+//     $scope.recordErrors = function (error) {
+//         debugger
+//         $scope.errors = [];
+//         if (error.data.errors.title) {
+//             $scope.errors.push(error.data.errors.title[0]);
+//         }
+//
+//         if (error.data.errors.description) {
+//             $scope.errors.push(error.data.errors.description[0]);
+//         }
+//     };
+//
+//     $scope.resetForm = function () {
+//         $scope.post.name = '';
+//         $scope.post.description = '';
+//         $scope.errors = [];
+//     };
+//
+//
+//     // initialize update action
+//     $scope.initEdit = function (index) {
+//         $scope.errors = [];
+//         $scope.edit_post = $scope.posts[index];
+//         $("#edit_post").modal('show');
+//     };
+//
+//     // update the given post
+//     $scope.updatePost = function () {
+//         $http.patch('/post/' + $scope.edit_post.id, {
+//             title: $scope.edit_post.title,
+//             description: $scope.edit_post.description
+//         }).then(function success(e) {
+//             $scope.errors = [];
+//             $("#edit_post").modal('hide');
+//         }, function error(error) {
+//             $scope.recordErrors(error);
+//         });
+//     };
+//
+//
+//     $scope.deletePost = function (index) {
+//
+//         var conf = confirm("Do you really want to delete this post?");
+//
+//         if (conf === true) {
+//             $http.delete('/post/' + $scope.posts[index].id)
+//                 .then(function success(e) {
+//                     $scope.posts.splice(index, 1);
+//                 });
+//         }
+//     };
+//
+//
+// }]);
